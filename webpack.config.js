6067bd732eb829a9590423f0321de2970a27a374
Julio López Torres <julio@aircury.com>
2021-05-27 15:47:58 +0200

    Send fallbacks to `index.html` so react-router can take care

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ module.exports = {
         contentBase: path.join(__dirname, "./"),
         port: 3000,
         publicPath: "http://localhost:3000/dist/",
+        historyApiFallback: true,
         hotOnly: true
     },
     plugins: [new webpack.HotModuleReplacementPlugin(), new Dotenv()],
