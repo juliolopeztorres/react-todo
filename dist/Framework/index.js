138d593caadccf4252b1953df7ee97b9e8296b50
Julio López Torres <julio@aircury.com>
2021-11-09 17:23:48 +0100

    Add `build`

--- dist/Framework/index.js
+++ dist/Framework/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import RouteService from "./Service/RouteService";
-ReactDOM.render(React.createElement(BrowserRouter, null,
+ReactDOM.render(React.createElement(HashRouter, null,
     React.createElement(RouteService, null)), document.getElementById("app"));
 //# sourceMappingURL=index.js.map
